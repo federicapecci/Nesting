a1c1893dd166e3bb01f592866c2df49acd715f53
Federica Pecci <federica.pecci2@studio.unibo.it>
2020-02-12 17:56:45 +0100

    fix pushitemdown() (part I)

--- Nesting/Utilities.cs
+++ Nesting/Utilities.cs
@@ -1,9 +1,5 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nesting
 {
@@ -54,6 +50,7 @@ namespace Nesting
             else if (temporaryBin.Points.Count == 1 && //se il bin contiene 1 solo punto e quel punto è (0,0)
                 temporaryBin.Points.ElementAt(0).Pposition == 0 && 
                 temporaryBin.Points.ElementAt(0).Qposition == 0) {
+
                 temporaryBin.NestedItems = new List<NestedItem>
                 {
                     new NestedItem(temporaryItem)
@@ -67,27 +64,7 @@ namespace Nesting
                     }
                 };
 
-                //setto il punto ad usato
-                temporaryBin.Points.ElementAt(0).IsUsed = true;
-
-                //aggiungo 2 nuovi punti
-                temporaryBin.Points.Add(new Tuple()
-                {
-                    Pposition = 0,
-                    Qposition = temporaryItem.Height,
-                    IsUsed = false
-
-                });
-
-                temporaryBin.Points.Add(new Tuple()
-                {
-                    Pposition = temporaryItem.Width,
-                    Qposition = 0,
-                    IsUsed = false
-                });
-
-                //setto item a nestato
-                temporaryItem.IsRemoved = true;
+                HandleOperationsPostNestedItem(temporaryBin, temporaryItem, temporaryBin.Points.ElementAt(0));
                 return true;
             }
             else if (temporaryBin.Points.Count > 1)//se il bin contiene n punti
@@ -106,7 +83,7 @@ namespace Nesting
                             TLpPosition = feasiblePoint.Pposition,
                             TLqPosition = feasiblePoint.Qposition + temporaryItem.Height
                         };
-                        ComputeHatchedRegion(feasiblePoint, newNestedItem, temporaryBin, temporaryItem);
+                        feasiblePoint.HatchedRegion = GetHatchedRegion(feasiblePoint, newNestedItem, temporaryBin, temporaryItem);
                     }
                 }
 
@@ -250,7 +227,7 @@ namespace Nesting
         /// <param name="temporaryBin"></param>
         /// <param name="temporaryItem"></param>
         /// <returns></returns>
-        private float ComputeHatchedRegion(Tuple feasiblePoint, NestedItem newNestedItem, Bin<Tuple> temporaryBin, Item temporaryItem)
+        private float GetHatchedRegion(Tuple feasiblePoint, NestedItem newNestedItem, Bin<Tuple> temporaryBin, Item temporaryItem)
         {
             if (temporaryItem.Id <= 1)
             {
@@ -260,31 +237,49 @@ namespace Nesting
             }
             else
             {
-                PushItemDown(feasiblePoint, temporaryBin, temporaryItem, newNestedItem);
-                PushItemLeft(feasiblePoint, temporaryBin, temporaryItem, newNestedItem);
-                //scrivo qui come calcolare l'hatched region
-                return 0;
+                //se le pozioni finali non sono state calcolate significa che 
+                //nemmeno l'hatched region è stata calcolata.
+                //quindi procedo calcolandomela
+                if (feasiblePoint.pFinalPosition == 0 &&
+                    feasiblePoint.qFinalPosition == 0)
+                {
+                    PushItemDown(feasiblePoint, temporaryBin, temporaryItem, newNestedItem);
+                    PushItemLeft(feasiblePoint, temporaryBin, temporaryItem, newNestedItem);
+
+                    //controllo se l'oggettto, anche essendo stato spostato in basso a sintra, sborda
+                    if (IsBorderObserved(newNestedItem, temporaryBin.Height, temporaryBin.Width))
+                    {
+                        if (feasiblePoint.Pposition == feasiblePoint.pFinalPosition &&
+                            feasiblePoint.Qposition == feasiblePoint.qFinalPosition)
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            //TO DO calcolo hatched region
+                            float hatchedRegion = 3;
+                            return hatchedRegion;
+                        }
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                return feasiblePoint.HatchedRegion;
             }
         }
 
         private void PushItemDown(Tuple feasiblePoint, Bin<Tuple> temporaryBin, Item temporaryItem, NestedItem newNestedItem)
         {
-            //dichiaro booleano per vedere se ho già calcolato la PfinalPosition e la QfinalPosition
-            bool finalPositionAlreadyComputed = false;
-
-            if(feasiblePoint.Pposition == feasiblePoint.pFinalPosition &&
-                feasiblePoint.Qposition == feasiblePoint.qFinalPosition)
-            {
-                finalPositionAlreadyComputed = true;
-            }
 
             //lista delle possibili intersezioni tra item nuovo e item già in soluzione
             IList<NestedItem> possibleIntersectionItems = new List<NestedItem>();
 
             foreach (var nestedItem in temporaryBin.NestedItems)
             {
-                if (feasiblePoint.Pposition > nestedItem.TLpPosition &&
-                   feasiblePoint.Pposition < nestedItem.BRpPosition)
+                if (feasiblePoint.Pposition >= nestedItem.TLpPosition &&
+                   feasiblePoint.Pposition <= nestedItem.BRpPosition)
                 {
                     possibleIntersectionItems.Add(nestedItem);
                 }
@@ -295,15 +290,12 @@ namespace Nesting
 
             foreach (var possibleIntersectionItem in possibleIntersectionItems)
             {
-                if (feasiblePoint.Qposition > possibleIntersectionItem.TLqPosition)
+                if (feasiblePoint.Qposition >= possibleIntersectionItem.TLqPosition)
                 {
                     intersectionItems.Add(possibleIntersectionItem);
                 }
             }
 
-            //se non ha già calcolato la PfinalPosition e la QfinalPosition        
-            if (!finalPositionAlreadyComputed)
-            {
                 //3 possibili risultati
                 if (intersectionItems.Count == 0) //non ho intersezioni
                 {
@@ -332,12 +324,68 @@ namespace Nesting
                 }
 
                 feasiblePoint.qFinalPosition = newNestedItem.BLqPosition;
-            }
+            
         }
 
         private void PushItemLeft(Tuple feasiblePoint, Bin<Tuple> temporaryBin, Item temporaryItem, NestedItem newNestedItem)
         {
-            //feasiblePoint.PfinalPosition = newNestedItem.BLpPosition;
+           
+
+            //lista delle intersezioni tra item nuovo e item già in soluzione
+            IList<NestedItem> intersectedItems = new List<NestedItem>();
+
+            foreach (var nestedItem in temporaryBin.NestedItems)
+            {
+                if (feasiblePoint.Qposition <= nestedItem.TLqPosition &&
+                   feasiblePoint.Qposition >= nestedItem.BRqPosition &&
+                   feasiblePoint.Pposition >= nestedItem.BRpPosition)
+                {
+                    intersectedItems.Add(nestedItem);
+                }
+            }
+
+                //3 possibili risultati
+                if (intersectedItems.Count == 0) //non ho intersezioni
+                {
+                    newNestedItem.BLpPosition = 0;
+                    newNestedItem.TLpPosition = 0;
+                    newNestedItem.BRpPosition = temporaryItem.Width;
+                }
+                else if (intersectedItems.Count == 1) //1 sola intersezione
+                {
+                    float delta = feasiblePoint.Pposition - intersectedItems.ElementAt(0).Width;
+                    newNestedItem.BLpPosition -= delta;
+                    newNestedItem.TLpPosition -= delta;
+                    newNestedItem.BRpPosition -= delta;
+                }
+                else if (intersectedItems.Count > 1) //N intersezioni
+                {
+                    float widthSum = 0;
+                    foreach (var intersectedItem in intersectedItems)
+                    {
+                        widthSum += intersectedItem.Width;
+                    }
+                    float delta = feasiblePoint.Pposition - widthSum;
+                    newNestedItem.BLpPosition -= delta;
+                    newNestedItem.TLpPosition -= delta;
+                    newNestedItem.BRpPosition -= delta;
+                }
+
+                feasiblePoint.pFinalPosition = newNestedItem.BLpPosition;
+            
+
+        }
+
+        /// <summary>
+        /// Questo metodo controlla se le dimensione dell'item eccedono quelle del bin
+        /// </summary>
+        /// <param name="newNestedItem"></param>
+        /// <param name="temporaryBinHeight"></param>
+        /// <param name="temporaryBinWidth"></param>
+        /// <returns></returns>
+        bool IsBorderObserved(NestedItem newNestedItem, float temporaryBinHeight, float temporaryBinWidth)
+        {
+            return newNestedItem.TLqPosition < temporaryBinHeight && newNestedItem.BRpPosition < temporaryBinWidth;
         }
 
         /// <summary>
